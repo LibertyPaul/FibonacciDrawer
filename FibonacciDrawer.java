a57a33c7107eb9f712b73b40d8badf3d9d1d85c8
LibertyPaul <git@libertypaul.ru>
2015-01-31 18:26:04 +0300

    first commit

---
+++ FibonacciDrawer.java
@@ -0,0 +1,16 @@
+import java.awt.Canvas;
+
+
+public class FibonacciDrawer extends Canvas {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final int sequenceLength = 100;
+	
+	
+	
+
+}
